740d17a555c18d5dec56561b9858365c3cad4afd
alex-taxiera <alex.taxiera@gmail.com>
2018-12-07 18:55:17 -0500

    rewrite for subcommand system

--- src/commands/tag.js
+++ src/commands/tag.js
@@ -1,119 +1,196 @@
 const { Command } = require('eris-boiler')
 const { get } = require('superagent')
 
-module.exports = (bot) => new Command(bot, {
+const IMAGE_REGEXP = new RegExp(/^(https|http):?\/(.*).(png|jpeg|jpg|gif)/)
+
+module.exports = (bot) => new Command(
+  bot,
+  {
     name: 'tag',
+    description: 'Finds, Adds, Remove, or Edit tags',
     options: {
-        deleteResponse: false
+      parameters: ['query'],
+      deleteInvoking: false,
+      deleteResponse: false,
+      subCommands: [add, remove, update, list, search, info]
     },
-    description: 'Finds, Adds, Remove, or Edit tags\n',
-    run: async ({ msg, params }) => {
-        let { prefix } = await bot.dbm.getSettings(msg.channel.guild.id)
-        if (params.length !== 0) return await doAction(params, msg, bot)
-        else return `I\'m not sure if you\'re doing this correctly. Try doing '${prefix}help tag'`
+    run: async function ({ bot, msg, params }) {
+      const key = params[0]
+      const tag = await bot.tag.getTag(key)
+      if (!tag) return `Tag \`${key}\` doesn't exists`
+
+      const { body } = await get(tag.src)
+      const ext = tag.src.toLowerCase().match(IMAGE_REGEXP).pop()
+      bot.tag.incrementTagCount(tag.key).catch((error) => console.error('failed to count', error))
+
+      return { file: { file: body, name: `${tag.key}.${ext}` } }
     }
-})
+  }
+)
 
-async function doAction([cmd, ...values], msg, client) {
-    const key = values[0]
+const add = (bot) => new Command(
+  bot,
+  {
+    name: 'add',
+    description: 'Add a tag',
+    options: {
+      aliases: ['create'],
+      parameters: ['tag key']
+    },
+    run: async function ({ bot, msg, params }) {
+      const [key, url] = params
+      const src = msg.attachments.length > 0 ? msg.attachments[0].url : url
 
-    values.shift()
-    let value = undefined
-    if (values.length !== 0) value = values
-    if (msg.attachments.length !== 0) value = msg.attachments[0].url
+      if (!key) return 'Missing key!'
+      if (!src) return 'Please attach an image or specify a url!'
+      if (!IMAGE_REGEXP.test(src)) return 'Not an image!'
 
-    switch (cmd) {
-        case 'add':
-        case 'create':
-            if (!key) return 'Missing key!'
-            if (!value) return 'Missing value!'
-            const result = await client._tagger.add(msg.author.id, key, (Array.isArray(value) ? value.join(' ') : value))
-            if (result === false) return 'Current link doesn\'t exists... please try another.'
-            return `Added tag \`${key}\` with value of ${Array.isArray(value) ? value.join(' ') : value}`
-        case 'delete':
-        case 'remove':
-            if (!key) return 'Missing key!'
-            if (!(await client._tagger.get(key))) return 'Tag doesn\'t exist'
-            if (msg.author.id !== (await client._tagger.get(key)).id) return 'Nope'
+      try {
+        await get(src)
+      } catch (err) {
+        return 'Bad link!'
+      }
 
-            await client._tagger.remove(key)
-            return `Deleted tag \`${key}\``
-        case 'edit':
-        case 'update':
-            if (!key) return 'Missing key!'
-            if (!value) return 'Missing value!'
-            if (!(await client._tagger.get(key))) return 'Tag doesn\'t exist'
-            if (msg.author.id !== (await client._tagger.get(key)).id) return 'Nope'
-
-            await client._tagger.edit(key, (Array.isArray(value) ? value.join(' ') : value))
-            return `Updated tag \`${key}\` to ${Array.isArray(value) ? value.join(' ') : value}`
-        case 'list':
-        case 'tags':
-            const values = await client._tagger.grabUserTags(msg.author.id)
-            if (values === undefined) return 'No tags found for you.'
-
-            return { 
-                embed: {
-                    title: `${msg.author.username} Tags.`,
-                    description: values.map(({ key }) => key).join('\n')
-                } 
-            }
-        case 'search':
-        case 'find':
-            let tags = await client._tagger.grabTags(key)
-            if (tags.length === 0) return 'No tags found.'
-
-            return { 
-                embed: {
-                    title: `Found tags with relation of ${key}`,
-                    description: tags.map(({ key }) => key).join('\n')
-                } 
-            }
-        case 'info':
-        case 'about':
-            if (!key) return `Missing tag key!`
-            const tagInfo = await client._tagger.get(key)
-            if (!tagInfo) return `Tag \`${key}\` doesn't exists`
-            const user = client.users.get(tagInfo.id)
-
-            return {
-                embed: {
-                    author: {
-                        name: `${user.username}#${user.discriminator}`,
-                        icon_url: user.avatarURL
-                    },
-                    fields: [
-                        {
-                            name: 'Tag Name',
-                            value: tagInfo.key
-                        },
-                        {
-                            name: 'Tag Value',
-                            value: tagInfo.value
-                        },
-                        {
-                            name: 'Use Count',
-                            value: tagInfo.count
-                        }
-                    ]
-                }
-            }
-        default:
-            const tag = await client._tagger.get(cmd)
-            const isImagRegex = new RegExp(/^(https|http):?\/(.*).(png|jpeg|jpg|gif)/)
-
-            if (!tag) return `Tag \`${cmd}\` doesn't exists`
-            if (isImagRegex.test(tag.value)) return await grabImage(client, tag)
-            await client._tagger.count(tag.key)
-
-            return { content: `Tag \`${tag.key}\` found.\n${tag.value}` }
+      await bot.tag.addTag(msg.author.id, key, src)
+      return `Added tag \`${key}\``
     }
-}
+  }
+)
 
-async function grabImage(client, tag) {
-    const { body } = await get(tag.value)
-    const ext = tag.value.toLowerCase().match(/^https:?\/(.*).(png|jpeg|jpg|gif)/)[2]
-    await client._tagger.count(tag.key)
+const remove = (bot) => new Command(
+  bot,
+  {
+    name: 'remove',
+    description: 'Add a tag',
+    options: {
+      aliases: ['delete', 'del', 'rm'],
+      parameters: ['tag key']
+    },
+    run: async function ({ bot, msg, params }) {
+      const key = params[0]
+      const tag = await bot.tag.getTag(key)
+      if (!tag) return 'Tag doesn\'t exist'
+      if (msg.author.id !== tag.userId) return 'You cannot remove tags you do not own!'
 
-    return { content: `Tag \'${tag.key}\' found.`, file: { file: body, name: `loli_tagger.image.${ext}` } }
-}
+      await bot.tag.removeTag(key)
+      return `Deleted tag \`${key}\``
+    }
+  }
+)
+
+const update = (bot) => new Command(
+  bot,
+  {
+    name: 'update',
+    description: 'Update a tag',
+    options: {
+      aliases: ['edit'],
+      parameters: ['tag key']
+    },
+    run: async function ({ bot, msg, params }) {
+      const [key, url] = params
+      const src = msg.attachments.length > 0 ? msg.attachments[0].url : url
+
+      if (!key) return 'Missing key!'
+      const tag = await bot.tag.getTag(key)
+      if (!tag) return 'Tag doesn\'t exist'
+      if (msg.author.id !== tag.userId) return 'You cannot update tags you do not own!'
+      if (!src) return 'Please attach an image or specify a url!'
+      if (!IMAGE_REGEXP.test(src)) return 'Not an image!'
+
+      try {
+        await get(src)
+      } catch (err) {
+        return 'Bad link!'
+      }
+
+      await bot.tag.updateTag(key, src)
+      return `Updated tag \`${key}\``
+    }
+  }
+)
+
+const list = (bot) => new Command(
+  bot,
+  {
+    name: 'list',
+    description: 'List your tags',
+    run: async function ({ bot, msg, params }) {
+      const tags = await bot.tag.selectTagsForUser(msg.author.id)
+      if (!tags) return 'No tags found for you.'
+
+      return {
+        embed: {
+          title: `${msg.author.username} Tags.`,
+          description: tags.map(({ key }) => key).join('\n')
+        }
+      }
+    }
+  }
+)
+
+const search = (bot) => new Command(
+  bot,
+  {
+    name: 'search',
+    description: 'Search all tags',
+    options: {
+      aliases: ['find'],
+      parameters: ['query']
+    },
+    run: async function ({ bot, msg, params }) {
+      const query = params[0]
+      const tags = await bot.tag.searchLikeTags(query)
+      if (!tags || tags.length < 1) return 'No tags found.'
+
+      return {
+        embed: {
+          title: 'Search results',
+          description: tags.map(({ key }) => key).join('\n')
+        }
+      }
+    }
+  }
+)
+
+const info = (bot) => new Command(
+  bot,
+  {
+    name: 'info',
+    description: 'Get info on a tag.',
+    options: {
+      aliases: ['about'],
+      parameters: ['tag key']
+    },
+    run: async function ({ bot, msg, params }) {
+      const key = params[0]
+      const tag = await bot.tag.getTag(key)
+      if (!tag) return `Tag \`${key}\` doesn't exists`
+      const user = bot.users.get(tag.userId)
+
+      return {
+        embed: {
+          author: {
+            name: `${user.username}#${user.discriminator}`,
+            icon_url: user.avatarURL
+          },
+          thumbnail: { url: IMAGE_REGEXP.test(tag.src) ? tag.src : '' },
+          fields: [
+            {
+              name: 'Tag Name',
+              value: tag.key
+            },
+            {
+              name: 'Tag Source',
+              value: tag.src
+            },
+            {
+              name: 'Use Count',
+              value: tag.count
+            }
+          ]
+        }
+      }
+    }
+  }
+)
